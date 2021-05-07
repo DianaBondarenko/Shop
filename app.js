ec46259005012aaf36a920ae8a9a24a98a569d63
DianaBondarenko <dianabondarenko17@gmail.com>
2021-05-07 12:12:06 +0300

    .env

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const productRouter = require('./routes/productRouter.js');
 const orderRouter = require('./routes/orderRouter.js');
 const pool = require('./models/pool.js');
-const options = require('./optionsDB.js');
+const options = JSON.parse(process.env.DBOPTIONS);  //require('./optionsDB.js');
 const morgan = require('morgan'); // console.log query info
-const authMiddleWare = require('./common/middleware/authMiddleWare.js');
-const authDTO = require('./common/middleware/authDTO.js');
-const orderDTO = require('./common/middleware/orderDTO.js');
-const validator = require('express-joi-validation').createValidator({});
 const errorHandler = require('./common/middleware/errorHandler.js');
 const NotFound = require("./common/errors/notFound.js");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('dev'));
 app.use('/products', productRouter);
 app.use('/order', orderRouter);
-app.use((req, res)  => {
+app.use((req, res) => {
     throw new NotFound('Not found')
 });
 app.use(errorHandler);
